530aa746b45ea81f6b95c40a13542b0360f49d60
Mohamed Ayman <devel.mohamed.ayman@gmail.com>
2025-12-10 23:33:44 +0200

    feat: Enhance localization by adding Arabic and English translations for booking-related terms and UI elements, including login and theme options.

--- resources/views/admin/layouts/vertical/navbar.blade.php
+++ resources/views/admin/layouts/vertical/navbar.blade.php
@@ -59,27 +59,28 @@
                 <a class="nav-link dropdown-toggle hide-arrow btn btn-icon btn-text-secondary rounded-pill"
                     id="nav-theme" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <i class="icon-base ti tabler-sun icon-22px theme-icon-active text-heading"></i>
-                    <span class="d-none ms-2" id="nav-theme-text">Toggle theme</span>
+                    <span class="d-none ms-2" id="nav-theme-text">{{ __('Toggle theme') }}</span>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="nav-theme-text">
                     <li>
                         <button type="button" class="dropdown-item align-items-center active"
                             data-bs-theme-value="light" aria-pressed="false">
-                            <span><i class="icon-base ti tabler-sun icon-22px me-3" data-icon="sun"></i>Light</span>
+                            <span><i class="icon-base ti tabler-sun icon-22px me-3"
+                                    data-icon="sun"></i>{{ __('Light') }}</span>
                         </button>
                     </li>
                     <li>
                         <button type="button" class="dropdown-item align-items-center" data-bs-theme-value="dark"
                             aria-pressed="true">
                             <span><i class="icon-base ti tabler-moon-stars icon-22px me-3"
-                                    data-icon="moon-stars"></i>Dark</span>
+                                    data-icon="moon-stars"></i>{{ __('Dark') }}</span>
                         </button>
                     </li>
                     <li>
                         <button type="button" class="dropdown-item align-items-center" data-bs-theme-value="system"
                             aria-pressed="false">
                             <span><i class="icon-base ti tabler-device-desktop-analytics icon-22px me-3"
-                                    data-icon="device-desktop-analytics"></i>System</span>
+                                    data-icon="device-desktop-analytics"></i>{{ __('System') }}</span>
                         </button>
                     </li>
                 </ul>
@@ -179,14 +180,14 @@
                     <li>
                         <a class="dropdown-item" href="{{ route('profile.index') }}">
                             <i class="icon-base ti tabler-settings me-3 icon-md"></i><span
-                                class="align-middle">Settings</span>
+                                class="align-middle">{{ __('Settings') }}</span>
                         </a>
                     </li>
 
                     <li>
                         <div class="d-grid px-2 pt-2 pb-1">
                             <a class="btn btn-sm btn-danger d-flex" href="{{ route('logout') }}">
-                                <small class="align-middle">Logout</small>
+                                <small class="align-middle">{{ __('Logout') }}</small>
                                 <i class="icon-base ti tabler-logout ms-2 icon-14px"></i>
                             </a>
                         </div>
